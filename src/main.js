e4cec048be7219d9a60271c8cbaca5d6ee204381
沈佳杰 <shenjiajie@shenjiajiedeMacBook-Air.local>
2018-08-28 11:40:17 +0800

    init vue-cli

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import '@/styles/reset.css'
+import '@/styles/border.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
